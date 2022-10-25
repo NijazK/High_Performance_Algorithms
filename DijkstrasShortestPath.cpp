970942bb6bbab2f4e1a2af2ba0e67cc92797404b
Nijaz Kovacevic <nijaz.kovacevic94@gmail.com>
2022-10-25 16:04:26 -0700

    Added files

---
+++ DijkstrasShortestPath.cpp
@@ -0,0 +1,27 @@
+DijkstraShortestPath(startV) {
+   for each vertex currentV in graph {
+      currentV->distance = Infinity
+      currentV->predV = 0
+      Push currentV to unvisitedQueue
+   }
+
+   // startV has a distance of 0 from itself
+   startV->distance = 0
+
+   while (unvisitedQueue is not empty) {
+      // Visit vertex with minimum distance from startV
+      currentV = PopMin unvisitedQueue
+
+      for each vertex adjV adjacent to currentV {
+         edgeWeight = weight of edge from currentV to adjV
+         alternativePathDistance = currentV->distance + edgeWeight
+                  
+         // If shorter path from startV to adjV is found,
+         // update adjV's distance and predecessor
+         if (alternativePathDistance < adjV->distance) {
+            adjV->distance = alternativePathDistance
+            adjV->predV = currentV
+         }
+      }
+   }
+}
